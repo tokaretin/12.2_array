1ddfe7534f40476f9b84153b44a8bccaf3700c55
tokaretin <tokaretin@gmail.com>
2023-05-03 23:06:02 +0200

    Добавьте файлы проекта.

---
+++ 12.2_array/12.2_array.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    int pupils[5];
+    std::string name[] = {"Simona", "Lev", "Anna", "Ilia", "Karetin"};
+
+    for (int i = 0; i < 5; i++) {
+        std::cout << "Write mark for name #" << name[i] << '\n';
+        std::cin >> pupils[i];
+    }
+
+    for (int i = 0; i < 5; i++) {
+        std::cout << name[i] << " has mark " << pupils[i] << '\n';
+    }
+}
+
